b7bd53b4f7a743e74b10b224267bb34f40ebc0ff
cocobango <evyatar.comodo@gmail.com>
2015-08-29 02:35:10 -0400

    included more space for decimal fields representing precentages

--- wage_reports/reports/models.py
+++ wage_reports/reports/models.py
@@ -126,7 +126,7 @@ class Monthly_employer_data(models.Model):
     lower_tax_threshold = models.DecimalField(max_digits=11, decimal_places=2)
     upper_tax_threshold = models.DecimalField(max_digits=11, decimal_places=2)
     income_tax_threshold = models.DecimalField(max_digits=11, decimal_places=2)
-    exact_income_tax_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    exact_income_tax_percentage = models.DecimalField(max_digits=16, decimal_places=8)
 
     def save(self, commit=True, *args, **kwargs):
         if commit:
@@ -146,14 +146,17 @@ class Monthly_employer_data(models.Model):
     
 class Monthly_system_data(models.Model):
     """The parameters that are used for each month's calculations is called Monthly_system_data"""
-    vat_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    social_security_threshold = models.DecimalField(max_digits=11, decimal_places=2)
-    lower_employee_social_security_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    lower_employer_social_security_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    upper_employee_social_security_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    upper_employer_social_security_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True , blank=True)
+    for_month = models.IntegerField(default=0) 
+    for_year = models.IntegerField(default=0)     
+    vat_percentage = models.DecimalField(max_digits=16, decimal_places=8)
+    social_security_threshold = models.DecimalField(max_digits=16, decimal_places=8)
+    lower_employee_social_security_percentage = models.DecimalField(max_digits=16, decimal_places=8)
+    lower_employer_social_security_percentage = models.DecimalField(max_digits=16, decimal_places=8)
+    upper_employee_social_security_percentage = models.DecimalField(max_digits=16, decimal_places=8)
+    upper_employer_social_security_percentage = models.DecimalField(max_digits=16, decimal_places=8)
     maximal_sum_to_pay_social_security = models.DecimalField(max_digits=11, decimal_places=2)
-    income_tax_default = models.DecimalField(max_digits=11, decimal_places=2)
+    income_tax_default = models.DecimalField(max_digits=16, decimal_places=8)
 
 
 class Locked_months(models.Model):
