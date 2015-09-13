675052fb0133710b121807167683c96d8a356b38
cocobango <evyatar.comodo@gmail.com>
2015-09-13 04:34:24 -0400

    added entries creation ability by using factories

--- wage_reports/reports/factories.py
+++ wage_reports/reports/factories.py
@@ -1,3 +1,7 @@
+from decimal import *
+
+from django.utils import timezone
+
 import factory
 from . import models
 
@@ -31,6 +35,86 @@ class EmployeeFactory(factory.django.DjangoModelFactory):
     birthday = fake.simple_profile()['birthdate']
     government_id = random.randint(10000000,100000000)
 
+class MonthlyEmployerDataFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Monthly_employer_data 
+    employee = factory.SubFactory(EmployeeFactory)
+    created = timezone.now()
+    entered_by = 'admin'
+    is_approved = True
+    for_year = 0
+    for_month = 0
+    is_required_to_pay_vat = True
+    is_required_to_pay_income_tax = True
+    lower_tax_threshold = Decimal(0.05)
+    upper_tax_threshold = Decimal(0.1)
+    income_tax_threshold = Decimal(60000)
+    exact_income_tax_percentage = Decimal(0.15)
+
+class MonthlyEmployeeDataFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Monthly_employee_data 
+    employee = factory.SubFactory(EmployeeFactory)
+    created = timezone.now()
+    entered_by = 'admin'
+    is_approved = True
+    gross_payment = Decimal(1000)
+    salary = Decimal(900)
+    general_expenses = Decimal(100)
+    gross_or_cost = True
+    is_required_to_pay_social_security = True
+    is_employer_the_main_employer = True
+    gross_payment_from_others = Decimal(200)
+    for_year = 0
+    for_month = 0
 
 
+
+
+class MyGenerators(object):
+    """Functions that assist in creating factories that handle the constraints in my models' ".save()" method"""
+    def __init__(self):
+        super(MyGenerators, self).__init__()
     
+    def stub(self):
+        return True
+
+    def generateMonthlyEmployeeDataWithFactory(self, *args, **kwargs):
+        """I have to make sure a user has employer data before inserting employee data. 
+        After I do that, I use the kwargs given to create the monthly data objects"""
+        employeeData = kwargs.get('employeeData',{})
+        employerData = kwargs.get('employerData',{})
+        
+        if 'employee' not in employeeData:
+            return False
+        
+        if 'employee' not in employerData:
+            employerData['employee'] = employeeData['employee']
+        
+        try:
+            employerDataFromDb = models.Monthly_employer_data.objects.filter(employee=employeeData['employee'])[0]
+        except IndexError as e:
+            MonthlyEmployerDataFactory(**employerData)
+        
+        return MonthlyEmployeeDataFactory(**employeeData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+        
--- wage_reports/reports/tests.py
+++ wage_reports/reports/tests.py
@@ -1,11 +1,19 @@
+from decimal import *
+
 from django.test import TestCase
-from reports.models import Employer , Employee
+from django.utils import timezone
+
+from reports.models import Employer , Employee , Monthly_employee_data , Monthly_employer_data
 from django.contrib.auth.models import User
 from . import factories
 
-class EmployerTestCase(TestCase):
+class SetupTestCase(TestCase):
+    def __init__(self,*args, **kwargs):
+        super(SetupTestCase, self).__init__(*args,**kwargs)
+        self.myGenerator = factories.MyGenerators()
+        print('running setup test case')
     def setUp(self):
-        print('doing some setup')
+        pass
     def test_tests_are_realy_running(self):
         """test realy run"""
         self.assertTrue(True)
@@ -16,7 +24,7 @@ class EmployerTestCase(TestCase):
 
         #act
         latest_employer = Employer.objects.order_by('-id')[0]
-
+        
         #assert
         self.assertEqual(employer.id , latest_employer.id)
         self.assertEqual(employer.user.id , latest_employer.user.id)
@@ -37,10 +45,102 @@ class EmployerTestCase(TestCase):
         #arrange
         employer = factories.EmployerFactory()
         employee = factories.EmployeeFactory(employer=employer)
-        # employee = factories.EmployeeWithEmployerFactory(employer)
-
         #act
-        
-
         #assert
         self.assertEqual(employee.employer.id , employer.id)
+
+    def test_able_to_generate_multiple_employees_for_the_same_employer(self):
+        #arrange
+        employer = factories.EmployerFactory()
+        
+        #act
+        employee_1 = factories.EmployeeFactory(employer=employer)
+        employee_2 = factories.EmployeeFactory(employer=employer)
+        employee_3 = factories.EmployeeFactory(employer=employer)
+
+        #assert
+        self.assertEqual(employee_1.employer.id , employer.id)
+        self.assertEqual(employee_2.employer.id , employer.id)
+        self.assertEqual(employee_3.employer.id , employer.id)
+
+
+    def test_able_to_insert_monthly_employer_data(self):
+        #arrange
+        employer = factories.EmployerFactory()
+        employee = factories.EmployeeFactory(employer=employer)  
+
+        #act
+        monthly_employer_data = Monthly_employer_data.objects.create(employee=employee ,
+            created = timezone.now(),
+            entered_by = 'admin',
+            is_approved = True,
+            for_year = 0,
+            for_month = 0,
+            is_required_to_pay_vat = True,
+            is_required_to_pay_income_tax = True,
+            lower_tax_threshold = Decimal(0.05),
+            upper_tax_threshold = Decimal(0.1),
+            income_tax_threshold = Decimal(60000),
+            exact_income_tax_percentage = Decimal(0.15) 
+        )  
+
+        #assert
+        self.assertIsInstance(monthly_employer_data.id , int)
+        self.assertEqual(monthly_employer_data.employee.id , employee.id)
+
+
+    def test_able_to_insert_monthly_employer_data_with_factory(self):
+        #arrange
+        employer = factories.EmployerFactory()
+        employee = factories.EmployeeFactory(employer=employer)  
+
+        #act
+        monthly_employer_data = factories.MonthlyEmployerDataFactory(employee=employee)  
+
+        #assert
+        self.assertIsInstance(monthly_employer_data.id , int)
+        self.assertEqual(monthly_employer_data.employee.id , employee.id)
+
+    def test_able_to_use_my_generators_class(self):
+        self.assertTrue(self.myGenerator.stub())
+
+
+    def test_able_to_insert_monthly_employee_data_with_generator(self):
+        #arrange
+        employer = factories.EmployerFactory()
+        employee = factories.EmployeeFactory(employer=employer)  
+        employeeData = {
+            'employee' : employee,
+        }
+        #act
+        monthly_employee_data = self.myGenerator.generateMonthlyEmployeeDataWithFactory(employeeData=employeeData)  
+
+        #assert
+        self.assertIsInstance(monthly_employee_data.id , int)
+        self.assertEqual(monthly_employee_data.employee.id , employee.id)
+
+    def test_able_to_explicitly_set_attribute_to_monthly_employee_data(self):
+        #arrange
+        employer = factories.EmployerFactory()
+        employee = factories.EmployeeFactory(employer=employer)  
+        employeeData = {
+            'employee' : employee,
+            'salary' : 2000
+        }
+        #act
+        monthly_employee_data = self.myGenerator.generateMonthlyEmployeeDataWithFactory(employeeData=employeeData)  
+
+        #assert
+        self.assertEqual(monthly_employee_data.salary , 2000)
+
+
+
+class CalculationTestCase(TestCase):
+    def __init__(self,*args, **kwargs):
+        super(CalculationTestCase, self).__init__(*args,**kwargs)
+        self.myGenerator = factories.MyGenerators()
+    def setUp(self):
+        print('doing some setup')
+    def test_tests_are_realy_running(self):
+        """test realy run"""
+        self.assertTrue(True)
