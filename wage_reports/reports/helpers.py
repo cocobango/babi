4f01c37b41c3a131691aec109617ca1240bf2526
cocobango <evyatar.comodo@gmail.com>
2015-09-13 07:39:06 -0400

    treating numbers as decimals with 6 precision points

--- wage_reports/reports/helpers.py
+++ wage_reports/reports/helpers.py
@@ -1,3 +1,6 @@
+from decimal import *
+getcontext().prec = 6
+
 from django.utils import timezone
 
 def get_month_in_question_for_employee_locking():
@@ -34,7 +37,7 @@ def calculate_social_security_employee(overall_gross,social_security_threshold,l
     if is_employer_the_main_employer:
         updated_threshold = social_security_threshold
     else:
-        updated_threshold = social_security_threshold - gross_payment_from_others
+        updated_threshold = Decimal(social_security_threshold) - Decimal(gross_payment_from_others)
         if updated_threshold < 0:
             updated_threshold = 0
 
@@ -48,23 +51,23 @@ def calculate_social_security_generic(overall_gross,social_security_threshold,lo
         sum_to_calculate_as_upper_social_security_percentage = 0
     else:
         sum_to_calculate_as_lower_social_security_percentage = social_security_threshold
-        sum_to_calculate_as_upper_social_security_percentage = (overall_gross - social_security_threshold)
-        
-    diminished_sum = sum_to_calculate_as_lower_social_security_percentage * lower_social_security_percentage
-    standard_sum = sum_to_calculate_as_upper_social_security_percentage * upper_social_security_percentage
-    total = diminished_sum + standard_sum
+        sum_to_calculate_as_upper_social_security_percentage = Decimal(overall_gross) - Decimal(social_security_threshold)
+    
+    diminished_sum = Decimal(sum_to_calculate_as_lower_social_security_percentage) * Decimal(lower_social_security_percentage)
+    standard_sum = Decimal(sum_to_calculate_as_upper_social_security_percentage) * Decimal(upper_social_security_percentage)
+    total = Decimal(diminished_sum) + Decimal(standard_sum)
     return { 'sum_to_calculate_as_lower_social_security_percentage' : sum_to_calculate_as_lower_social_security_percentage , 'sum_to_calculate_as_upper_social_security_percentage' : sum_to_calculate_as_upper_social_security_percentage ,'diminished_sum' : diminished_sum , 'standard_sum' : standard_sum , 'total' : total}
 
 def calculate_income_tax(overall_gross,income_tax_threshold,lower_tax_threshold,upper_tax_threshold,is_required_to_pay_income_tax,exact_income_tax_percentage,accumulated_gross_including_this_month,accumulated_income_tax_not_including_this_month,vat_due_this_month):
     if not is_required_to_pay_income_tax:
         return 0
     if exact_income_tax_percentage > 0:
-        return (overall_gross + vat_due_this_month) * exact_income_tax_percentage
+        return Decimal(overall_gross) + Decimal(vat_due_this_month) * Decimal(exact_income_tax_percentage)
     if accumulated_gross_including_this_month <= income_tax_threshold:
-        return ( accumulated_gross_including_this_month * lower_tax_threshold ) - accumulated_income_tax_not_including_this_month
-    standard_sum = (accumulated_gross_including_this_month - income_tax_threshold) * upper_tax_threshold
-    diminished_sum = income_tax_threshold * lower_tax_threshold
-    return standard_sum + diminished_sum - accumulated_income_tax_not_including_this_month
+        return Decimal(accumulated_gross_including_this_month) * Decimal(lower_tax_threshold) - Decimal(accumulated_income_tax_not_including_this_month)
+    standard_sum = Decimal(accumulated_gross_including_this_month) - Decimal(income_tax_threshold) * Decimal(upper_tax_threshold)
+    diminished_sum = Decimal(income_tax_threshold) * Decimal(lower_tax_threshold)
+    return Decimal(standard_sum) + Decimal(diminished_sum) - Decimal(accumulated_income_tax_not_including_this_month)
 
 def calculate_output_tax(overall_gross,vat_percentage,is_required_to_pay_vat):
     if not is_required_to_pay_vat:
@@ -72,5 +75,5 @@ def calculate_output_tax(overall_gross,vat_percentage,is_required_to_pay_vat):
     return overall_gross * vat_percentage
 
 def calculate_monthly_net(overall_gross, output_tax, social_security_employee, income_tax ):
-    return overall_gross + output_tax - social_security_employee - income_tax
+    return Decimal(overall_gross) + Decimal(output_tax) - Decimal(social_security_employee) - Decimal(income_tax)
 
